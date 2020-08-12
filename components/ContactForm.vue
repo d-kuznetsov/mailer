7f84bf61a7bf9f8dff22926ec1dfc0900863ad2f
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-08-12 22:01:27 +0200

    add form validation

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,9 +1,16 @@
 <template>
   <section>
-    <form @submit.prevent="submit">
+    <form
+      :class="{ 'form-error': submitStatus === 'INPUT_ERROR' }"
+      @submit.prevent="submit"
+    >
       <div class="mb-4">
         <input
           v-model="name"
+          :class="{
+            'input-error': $v.name.$invalid,
+            'input-succes': submitStatus == 'SUCCES',
+          }"
           class="form-input focus:outline-none focus:shadow-outline"
           type="text"
           name="name"
@@ -13,6 +20,10 @@
       <div class="mb-4">
         <input
           v-model="email"
+          :class="{
+            'input-error': $v.email.$invalid,
+            'input-succes': submitStatus == 'SUCCES',
+          }"
           class="form-input focus:outline-none focus:shadow-outline"
           type="text"
           name="email"
@@ -20,18 +31,36 @@
         />
       </div>
       <div class="mb-4">
+        <input
+          v-model="subject"
+          :class="{
+            'input-error': $v.subject.$invalid,
+            'input-succes': submitStatus == 'SUCCES',
+          }"
+          class="form-input focus:outline-none focus:shadow-outline"
+          type="text"
+          name="subject"
+          placeholder="Subject:"
+        />
+      </div>
+      <div class="mb-4">
         <textarea
           v-model="message"
+          :class="{
+            'input-error': $v.message.$invalid,
+            'input-succes': submitStatus == 'SUCCES',
+          }"
           class="form-input focus:outline-none focus:shadow-outline"
           name="message"
           rows="7"
-          placeholder="Message:"
+          placeholder="Message (from 10 characters):"
         ></textarea>
       </div>
       <div>
         <button
           class="hover:bg-blue-700 focus:outline-none focus:shadow-outline px-4 py-2 font-bold text-white bg-blue-500 rounded"
           type="submit"
+          :disabled="submitStatus == 'PENDING'"
         >
           Submit
         </button>
@@ -41,29 +70,69 @@
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, email, minLength } from 'vuelidate/lib/validators'
+
 export default {
+  mixins: [validationMixin],
   data() {
     return {
       name: '',
       email: '',
       message: '',
+      subject: '',
+      submitStatus: null,
     }
   },
   methods: {
     submit() {
-      const { name, email, message } = this
-      this.$axios
-        .post(process.env.MAIL_API_URL, {
-          name,
-          email,
-          message,
-        })
-        .then((response) => {
-          console.log(response.data)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        this.submitStatus = 'INPUT_ERROR'
+      } else {
+        const { name, email, subject, message } = this
+
+        this.submitStatus = 'PENDING'
+        this.$axios
+          .post(process.env.MAIL_API_URL, {
+            name,
+            email,
+            subject,
+            message,
+          })
+          .then(() => {
+            this.submitStatus = 'SUCCES'
+            setTimeout(() => {
+              this.submitStatus = null
+              this.clean()
+            }, 5000)
+          })
+          .catch(() => {
+            this.submitStatus = 'SERVER_ERROR'
+          })
+      }
+    },
+    clean() {
+      this.name = ''
+      this.email = ''
+      this.subject = ''
+      this.message = ''
+    },
+  },
+  validations: {
+    name: {
+      required,
+    },
+    email: {
+      required,
+      email,
+    },
+    subject: {
+      required,
+    },
+    message: {
+      required,
+      minLength: minLength(10),
     },
   },
 }
@@ -72,5 +141,12 @@ export default {
 <style>
 .form-input {
   @apply shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight appearance-none;
+}
+
+.form-error .input-error {
+  @apply border-red-700;
+}
+.input-succes {
+  @apply border-green-700;
 }
 </style>
