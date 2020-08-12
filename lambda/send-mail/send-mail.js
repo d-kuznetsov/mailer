eef9c98e6bc209e40a723774c9785d5a2191cde6
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-08-12 22:44:08 +0200

    send mail to https://mailtrap.io/

--- lambda/send-mail/send-mail.js
+++ lambda/send-mail/send-mail.js
@@ -1,10 +1,33 @@
+const nodemailer = require('nodemailer')
+
 // eslint-disable-next-line
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   try {
-    const { name, email, message } = JSON.parse(event.body)
+    const { name, email, subject, message } = JSON.parse(event.body)
+    const transport = nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: '31e990d1f69827',
+        pass: 'ccd2d6c63a8805',
+      },
+    })
+
+    const info = await transport.sendMail({
+      from: 'Sender Name <sender@example.com>',
+      to: `${email}, sender@example.com`,
+      subject: `${subject} ${new Date().toLocaleString()}`,
+      html: `
+        <div>
+          <p>${name}</p>
+          <p>${message}</p>
+        </div>
+      `,
+    })
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ name, email, message }),
+      body: info.messageId,
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
